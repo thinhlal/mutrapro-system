97dc941f32189485df1cd3023d8f0de6b64f5bce
DESKTOP-FURKJEG\AnhMinh <anhminh0026@gmail.com>
2025-09-06 13:38:51 +0700

    Add routing and styling for HomePage; include Ant Design and global styles;

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+// Import styles
+import "antd/dist/reset.css";
+import "./styles/global.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
